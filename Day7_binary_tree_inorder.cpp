0f498577ac3eaf25cf74b32ab6fc12b26586f44f
Parixit Chauhan <parixitchauhan2007@gmail.com>
2026-03-17 20:32:50 +0530

    day 7 done

--- Day7_binary_tree_inorder.cpp
+++ Day7_binary_tree_inorder.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+#include <vector>
+using namespace std;
 class Solution {
 public:
     void inorder(TreeNode* root,vector<int>& ans)
